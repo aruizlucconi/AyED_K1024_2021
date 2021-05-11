6d84031d9db87cbffbd147e42d581c66607e3b2b
aruizlucconi <83124636+aruizlucconi@users.noreply.github.com>
2021-05-11 20:50:19 -0300

    Add files via upload

---
+++ TP1/Ejercicio a/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    float vel;
+    cout << "Ingrese una velocidad (en Km/h)" << endl;
+    cin >> vel;
+    cout << "La velociad ingresada equivale a " << vel * 5/18 << " m/s"<< endl;
+    return 0;
+}
