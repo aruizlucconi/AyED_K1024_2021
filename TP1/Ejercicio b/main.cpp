4ea34d6cbd9bf763894e72f948321a26fa0f03db
aruizlucconi <83124636+aruizlucconi@users.noreply.github.com>
2021-05-12 21:17:19 -0300

    Add files via upload

---
+++ TP1/Ejercicio b/main.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    float m1,m2,b1,b2,x,y;
+    cout << "Ingrese pendiente y ordenada al origen de la primer recta" << endl;
+    cout << "m1:" ;
+    cin >> m1;
+    cout << "b1:" ;
+    cin >> b1;
+
+    cout << "Ingrese pendiente y ordenada al origen de la segunda recta" << endl;
+    cout << "m2:" ;
+    cin >> m2;
+    cout << "b2:" ;
+    cin >> b2;
+
+    if (m1==m2 && b1==b2)
+
+            cout << "Las rectas son iguales" << endl;
+    else
+        {
+            if (m1==m2)
+                cout << "Las rectas no se intersectan, son paralelas" << endl;
+            else
+                {
+                    x= (b2-b1)/(m1-m2);
+                    y= m1*x+b1;
+                    cout<< "Las rectas intersectan en el punto (" << x <<" ; " << y <<")"<< endl;
+                }
+        }
+
+
+
+
+
+    return 0;
+}
